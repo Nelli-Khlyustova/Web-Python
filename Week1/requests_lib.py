2050c4d3f0400adc1ae1048551da850d089071ab
NELLI KHLYUSTOVA <nellikhlyustova@gmail.com>
2020-07-04 22:11:22 +0300

    fix

--- Week1/requests_lib.py
+++ Week1/requests_lib.py
@@ -1,4 +1,5 @@
 import requests
+import json
 
 r = requests.get('http://httpbin.org/get')
 print(r.text)
@@ -11,4 +12,9 @@ r = requests.get('http://httpbin.org/get', params=payload)
 print(r.text)
 
 r = requests.post('http://httpbin.org/post', data={'key': 'value'})
+print(r.text)
+
+url = 'http://httpbin.org/post'
+r = requests.post(url, data=json.dumps({'key': 'value'}))
+r = requests.post(url, json={'key': 'value'})
 print(r.text)
