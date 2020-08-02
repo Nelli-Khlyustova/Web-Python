89b250b388e78268c586dbfe726f7d3c7d0569a4
NELLI KHLYUSTOVA <nellikhlyustova@gmail.com>
2020-08-02 18:58:38 +0300

    week 2

---
+++ Week2/wikik_parser/soup_parser.py
@@ -0,0 +1,26 @@
+from bs4 import BeautifulSoup
+import unittest
+
+def parse(path_to_file):
+    with open(path_to_file, encoding='UTF-8') as data:
+        soup = BeautifulSoup(data, 'lxml')
+    body = soup.find(id='bodyContent')
+    return [imgs, headers, linkslen, lists]
+
+
+class TestParse(unittest.TestCase):
+    def test_parse(self):
+        test_cases = (
+            ('wiki/Stone_Age', [13, 10, 12, 40]),
+            ('wiki/Brain', [19, 5, 25, 11]),
+            ('wiki/Artificial_intelligence', [8, 19, 13, 198]),
+            ('wiki/Python_(programming_language)', [2, 5, 17, 41]),
+            ('wiki/Spectrogram', [1, 2, 4, 7]),)
+
+        for path, expected in test_cases:
+            with self.subTest(path=path, expected=expected):
+                self.assertEqual(parse(path), expected)
+
+
+if __name__ == '__main__':
+    unittest.main()
