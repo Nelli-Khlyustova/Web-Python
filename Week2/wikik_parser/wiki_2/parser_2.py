16fa193072519e3468811bd1844b5e59486ec97a
NELLI KHLYUSTOVA <nellikhlyustova@gmail.com>
2020-08-03 14:17:00 +0300

    wiki parser 2

--- Week2/wikik_parser/wiki_2/parser_2.py
+++ Week2/wikik_parser/wiki_2/parser_2.py
@@ -1,90 +1,156 @@
-from bs4 import BeautifulSoup
 import unittest
+import os
+import re
 
-
-def get_imgs_count(body):
-    count = 0
-    tags = body('img')
-
-    for tag in tags:
-        if 'width' in tag.attrs:
-            if int(tag['width']) >= 200:
-                count += 1
-    return count
-
-
-def get_headers_count(body):
-    count = 0
-    tags = {}
-    for i in range(1, 7):
-        tags['h{}'.format(i)] = body('h{}'.format(i))
-
-    for head in tags.keys():
-        for tag in tags[head]:
-            for child in list(tag.children):
-                if str(child.string)[0] in ['E', 'T', 'C']:
-                    count += 1
-
-    return count
-
-
-def get_link_count(links_list):
-    max_len = 1
-
-    for i in range(len(links_list)):
-        if str(links_list[i])[1] == 'a':
-            max_len += 1
-        else:
-            break
-
-    return max_len
-
-def get_links_len(body):
-    link_len = 0
-    tags = body('a')
-    for tag in tags:
-        tmp_len = get_link_count(tag.find_next_siblings())
-        if tmp_len > link_len:
-            link_len = tmp_len
-
-    return link_len
-
-
-def get_lists_count(body):
-    count = 0
-    tags = body.find_all(['ul', 'ol'])
-
-    for tag in tags:
-        if not tag.find_parents(['ul', 'ol']):
-            count += 1
-
-    return count
+from bs4 import BeautifulSoup
 
 
 def parse(path_to_file):
-    with open(path_to_file, encoding='UTF-8') as data:
-        soup = BeautifulSoup(data, 'lxml')
-    body = soup.find(id='bodyContent')
-    imgs = get_imgs_count(body)  # Количество картинок (img) с шириной (width) не меньше 200
-    headers = get_headers_count(body)  # Количество заголовков, первая буква текста внутри которого: E, T или C
-    linkslen = get_links_len(body)  # Длина максимальной последовательности ссылок, между которыми нет других тегов
-    lists = get_lists_count(body)  # Количество списков, не вложенных в другие списки
+    with open(path_to_file, encoding="utf-8") as file:
+        soup = BeautifulSoup(file, "lxml")
+        body = soup.find(id="bodyContent")
+
+    imgs = len(body.find_all('img', width=lambda x: int(x or 0) > 199))
+
+    headers = sum(1 for tag in body.find_all(
+        ['h1', 'h2', 'h3', 'h4', 'h5', 'h6']) if tag.get_text()[0] in "ETC")
+
+    lists = sum(
+        1 for tag in body.find_all(['ol', 'ul']) if not tag.find_parent(['ol', 'ul']))
+
+    linkslen = 0
+
+    for a in body.find_all('a'):
+        current_streak = 1
+
+        for tag in a.find_next_siblings():
+            if tag.name == 'a':
+                current_streak += 1
+            else:
+                break
+
+        linkslen = current_streak if current_streak > linkslen else linkslen
 
     return [imgs, headers, linkslen, lists]
 
 
-class TestParse(unittest.TestCase):
-    def test_parse(self):
-        test_cases = (
-            ('wiki/Stone_Age', [13, 10, 12, 40]),
-            ('wiki/Brain', [19, 5, 25, 11]),
-            ('wiki/Artificial_intelligence', [8, 19, 13, 198]),
-            ('wiki/Python_(programming_language)', [2, 5, 17, 41]),
-            ('wiki/Spectrogram', [1, 2, 4, 7]),)
+def get_statistics(path, start_page, end_page):
+    """собирает статистику со страниц, возвращает словарь, где ключ - название страницы,
+    значение - список со статистикой страницы"""
+    pages = build_bridge(path, start_page, end_page)
+    statistic = {}
 
-        for path, expected in test_cases:
-            with self.subTest(path=path, expected=expected):
-                self.assertEqual(parse(path), expected)
+    for page in pages:
+        statistic[page] = parse(os.path.join(path, page))
+
+    return statistic
+
+
+def get_links(path, page):
+    """возвращает множество названий страниц, ссылки на которые содержатся в файле page"""
+
+    with open(os.path.join(path, page), encoding="utf-8") as file:
+        links = set(re.findall(r"(?<=/wiki/)[\w()]+", file.read()))
+        if page in links:
+            links.remove(page)
+    return links
+
+
+def get_backlinks(path, end_page, unchecked_pages, checked_pages, backlinks):
+    """возвращает словарь обратных ссылок (ключ - страница, значение - страница
+    с которой возможен переход по ссылке на страницу, указанную в ключе)"""
+
+    if end_page in checked_pages or not checked_pages:
+        return backlinks
+
+    new_checked_pages = set()
+
+    for checked_page in checked_pages:
+        unchecked_pages.remove(checked_page)
+        linked_pages = get_links(path, checked_page) & unchecked_pages
+
+        for linked_page in linked_pages:
+            backlinks[linked_page] = backlinks.get(linked_page, checked_page)
+            new_checked_pages.add(linked_page)
+
+    checked_pages = new_checked_pages & unchecked_pages
+
+    return get_backlinks(path, end_page, unchecked_pages, checked_pages, backlinks)
+
+
+def build_bridge(path, start_page, end_page):
+    """возвращает список страниц, по которым можно перейти по ссылкам со start_page на
+    end_page, начальная и конечная страницы включаются в результирующий список"""
+
+    backlinks = \
+        get_backlinks(path, end_page, set(os.listdir(path)), {start_page, }, dict())
+
+    current_page, bridge = end_page, [end_page]
+
+    while current_page != start_page:
+        current_page = backlinks.get(current_page)
+        bridge.append(current_page)
+
+    return bridge[::-1]
+
+STATISTICS = {
+    'Artificial_intelligence': [8, 19, 13, 198],
+    'Binyamina_train_station_suicide_bombing': [1, 3, 6, 21],
+    'Brain': [19, 5, 25, 11],
+    'Haifa_bus_16_suicide_bombing': [1, 4, 15, 23],
+    'Hidamari_no_Ki': [1, 5, 5, 35],
+    'IBM': [13, 3, 21, 33],
+    'Iron_Age': [4, 8, 15, 22],
+    'London': [53, 16, 31, 125],
+    'Mei_Kurokawa': [1, 1, 2, 7],
+    'PlayStation_3': [13, 5, 14, 148],
+    'Python_(programming_language)': [2, 5, 17, 41],
+    'Second_Intifada': [9, 13, 14, 84],
+    'Stone_Age': [13, 10, 12, 40],
+    'The_New_York_Times': [5, 9, 8, 42],
+    'Wild_Arms_(video_game)': [3, 3, 10, 27],
+    'Woolwich': [15, 9, 19, 38]}
+
+TESTCASES = (
+    ('wiki/', 'Stone_Age', 'Python_(programming_language)',
+     ['Stone_Age', 'Brain', 'Artificial_intelligence', 'Python_(programming_language)']),
+
+    ('wiki/', 'The_New_York_Times', 'Stone_Age',
+     ['The_New_York_Times', 'London', 'Woolwich', 'Iron_Age', 'Stone_Age']),
+
+    ('wiki/', 'Artificial_intelligence', 'Mei_Kurokawa',
+     ['Artificial_intelligence', 'IBM', 'PlayStation_3', 'Wild_Arms_(video_game)',
+      'Hidamari_no_Ki', 'Mei_Kurokawa']),
+
+    ('wiki/', 'The_New_York_Times', "Binyamina_train_station_suicide_bombing",
+     ['The_New_York_Times', 'Second_Intifada', 'Haifa_bus_16_suicide_bombing',
+      'Binyamina_train_station_suicide_bombing']),
+
+    ('wiki/', 'Stone_Age', 'Stone_Age',
+     ['Stone_Age', ]),
+)
+
+
+class TestBuildBrige(unittest.TestCase):
+    def test_build_bridge(self):
+        for path, start_page, end_page, expected in TESTCASES:
+            with self.subTest(path=path,
+                              start_page=start_page,
+                              end_page=end_page,
+                              expected=expected):
+                result = build_bridge(path, start_page, end_page)
+                self.assertEqual(result, expected)
+
+
+class TestGetStatistics(unittest.TestCase):
+    def test_build_bridge(self):
+        for path, start_page, end_page, expected in TESTCASES:
+            with self.subTest(path=path,
+                              start_page=start_page,
+                              end_page=end_page,
+                              expected=expected):
+                result = get_statistics(path, start_page, end_page)
+                self.assertEqual(result, {page: STATISTICS[page] for page in expected})
 
 
 if __name__ == '__main__':
